0ace7a84ee6a039a24c4f02e09c0765c6f40678a
KanPhacharawat <83383938+KanPhacharawat@users.noreply.github.com>
2025-04-26 14:37:07 +0700

    based

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "module/interface.h"
+
+int main(void){
+    displayMenu();
+    return 0;
+}
---
+++ module/interface.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void displayMenu(){
+    printf("File System\n");
+}
